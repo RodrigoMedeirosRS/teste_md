754aa01074b44eaab012a8b52374ff94bea2a380
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2022-04-24 09:57:33 -0300

    ajuestes debug windows

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 make -f $GENDEV/sgdk/mkfiles/Makefile.rom clean all
+
+C:\sgdk\bin\make -f C:\sgdk\makefile.gen
--- debug.sh
+++ debug.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 make -f $GENDEV/sgdk/mkfiles/Makefile.rom clean all
 mednafen ./out/rom.bin
+
+C:/sgdk/mednafen/mednafen.exe ./out/rom.bin
